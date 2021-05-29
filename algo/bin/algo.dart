187ecd85f258ef9dde2fdf6bf960c4309998e4c0
kitoko2 <dogbo804@gmail.com>
2021-05-29 15:00:24 +0000

    batle nan algo

--- algo/bin/algo.dart
+++ algo/bin/algo.dart
@@ -1,34 +1,16 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  var indicatif = false;
   print('hello entrer votre numero');
   var r = stdin.readLineSync();
-  var result = '';
 
   if (r.isEmpty) {
     print('numero vide');
     exit(1);
   }
 
-  for (var i = 0; i < r.length; i++) {
-    if (i == 0 || i == 1) {
-      if (r[i] == '1' || r[i] == '1') {
-        indicatif = true;
-      } // pour pouvoir enlever le 1 qui est l'indicatif
-    }
-    if (r[i] != '+' &&
-        indicatif == false &&
-        r[i] != ' ' &&
-        r[i] != '-' &&
-        r[i] != '.' &&
-        r[i] != '(' &&
-        r[i] != ')') {
-      result += (r[i]);
-    }
-    indicatif = false;
-    // pour que tous les 1 ne soit pas pris comme les indicatifs
-  }
+  r = r.replaceAll(new RegExp(r'[^\w\s/]+'), '');
+  r = r.replaceAll(new RegExp(r'^1'), '');
+
   print('ton numero sont nettoyage est :$r');
-  print('ton numero apres netoyage est : $result');
 }
